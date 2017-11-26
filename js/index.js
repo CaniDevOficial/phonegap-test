f4a732c45647126e6849fdc010c75dceec3afefb
CaniDevOficial <canidev@gmail.com>
2017-11-26 17:02:13 +0100

    test

--- js/index.js
+++ js/index.js
@@ -27,10 +27,10 @@ var app = {
 				preferFrontCamera : false, // iOS and Android
 				showFlipCameraButton : true, // iOS and Android
 				showTorchButton : true, // iOS and Android
-				torchOn: true, // Android, launch with the torch switched on (if available)
+				torchOn: false, // Android, launch with the torch switched on (if available)
 				prompt : "Place a barcode inside the scan area", // Android
-				resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-				formats : "CODE_39,CODE_128", // default: all but PDF_417 and RSS_EXPANDED
+				resultDisplayDuration: 0, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+				formats : "EAN_13,CODE_39,CODE_128", // default: all but PDF_417 and RSS_EXPANDED
 				disableAnimations : true, // iOS
 				disableSuccessBeep: false // iOS and Android
 			}
