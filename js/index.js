036619571ebefc71f3a53959e2b3859465606c1d
CaniDevOficial <canidev@gmail.com>
2017-11-26 20:25:46 +0100

    test

--- js/index.js
+++ js/index.js
@@ -1,18 +1,51 @@
 
 var app = {
+	rspUrl: 'https://www.canidev.com/barcode/jsonrpc.php',
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+		//document.addEventListener('DOMContentLoaded', this.onDeviceReady, false);
     },
 
     onDeviceReady: function() {
-		var launchDemoButton = document.getElementById('demo');
+		/*var launchDemoButton = document.getElementById('demo');
 		launchDemoButton.addEventListener('click', function() {
 			app.demo();
 			return false;
+		});*/
+		
+		$.ajax({
+			url 	: app.rspUrl,
+			type	: 'POST',
+			dataType: 'json',
+			success	: function(data) {
+				if(data.items)
+				{
+					$('.app').html('');
+
+					var $table = $('<table></table>').appendTo('.app');
+					
+					$.each(data.items, function(i, row) {
+						$table.append('<tr>' +
+							'<td class="title">' + row.name + '<dfn>Ref: ' + row.ref + '</dfn></td>' +
+							'<td class="ref">' + row.ref + '</td>' +
+							'<td class="barcode"><canvas id="barcode-model-' + i + '"></canvas></td>' +
+						'</tr>');
+						
+						JsBarcode('#barcode-model-' + i, row.ean, {
+							fontSize: 11,
+							lineColor: '#000',
+							height: 35
+						});
+					});
+				}
+			},
+			error	: function() {
+				alert('Error de conexion');
+			}
 		});
 	},
 	
-	demo: function() {
+	/*demo: function() {
 		cordova.plugins.barcodeScanner.scan(
 			function (result) {
 				alert("We got a barcode\n" +
@@ -35,7 +68,7 @@ var app = {
 				disableSuccessBeep: false // iOS and Android
 			}
 		);
-    }
+    }*/
 };
 
 app.initialize();
