33118415d84b944183d2c661a95f3561ed5f973b
CaniDevOficial <canidev@gmail.com>
2017-11-26 15:41:05 +0100

    test

--- js/index.js
+++ js/index.js
@@ -6,11 +6,13 @@ var app = {
 
     onDeviceReady: function() {
 		var launchDemoButton = document.getElementById('demo');
-		launchDemoButton.addEventListener('click', app.demo, false);
+		launchDemoButton.addEventListener('click', function() {
+			app.demo;
+			return false;
+		});
 	},
 	
 	demo: function() {
-		console.log(cordova.plugins);
 		cordova.plugins.barcodeScanner.scan(
 			function (result) {
 				alert("We got a barcode\n" +
@@ -22,13 +24,13 @@ var app = {
 				alert("Scanning failed: " + error);
 			},
 			{
-				preferFrontCamera : true, // iOS and Android
+				preferFrontCamera : false, // iOS and Android
 				showFlipCameraButton : true, // iOS and Android
 				showTorchButton : true, // iOS and Android
 				torchOn: true, // Android, launch with the torch switched on (if available)
 				prompt : "Place a barcode inside the scan area", // Android
 				resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-				formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+				formats : "EAN_13,CODE_39,CODE_128", // default: all but PDF_417 and RSS_EXPANDED
 				disableAnimations : true, // iOS
 				disableSuccessBeep: false // iOS and Android
 			}
